8e6be3a6a34d46537d46229553941edced02d719
cheang120 <babycode.2023@gmail.com>
2024-04-08 23:33:39 +0800

    contact

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -73,7 +73,7 @@ const Contact = () => {
                 </span>
                 <span>
                   <FaEnvelope />
-                  <p>carrey2024@outlook.com</p>
+                  <p>carrey2024@gmail.com</p>
                 </span>
                 <span>
                   <GoLocation />
